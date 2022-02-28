991f9cfadafbdcd7577baf8fa3f8d9432c893658
Matheus Ferreira <matheus.fer33@gmail.com>
2022-02-28 17:22:55 -0300

    upload do script atual. mais updates para vir

--- script.js
+++ script.js
@@ -61,14 +61,36 @@ let track = document.createElement('audio');
 
 //funcao carregar a musica
 function load_track(index_no) {
+    clearInterval(timer);
+    reset_slider();
     track.src = all_song[index_no].path;
     title.innerHTML = all_song[index_no].name;
     track_image.src = all_song[index_no].img;
     artist.innerHTML = all_song[index_no].singer;
     track.load();
+
+    total.innerHTML = all_song.length;
+    present.innerHTML = index_no + 1;
+
+    timer = setInterval(range_slider , 1000);
 }
 
 load_track(index_no);
+
+
+//mutar o som
+function mute_sound() {
+    track.volume = 0;
+    volume.value = 0;
+    volume_show.innerHTML = 0
+}
+
+
+//resetar slider
+function reset_slider() {
+    slider.value = 0
+}
+
 
 // checando se a musica esta tocando
 function justplay() {
@@ -95,7 +117,68 @@ function pausesong() {
 
 //proxima musica
 function next_song() {
-    index_no += 1;
-    load_track(index_no);
+    if (index_no < all_song.length - 1) {
+        index_no += 1;
+        load_track(index_no);
+        playsong();
+    } else {
+        index_no = 0;
+        load_track(index_no);
+        playsong;
+    }
+}
 
+//musica anterior
+function previous_song() {
+    if (index_no > 0) {
+        index_no -= 1;
+        load_track(index_no);
+        playsong();
+    } else {
+        index_no = all_song.length;
+        load_track(index_no);
+        playsong;
+    }
+}
+
+// alterar volume
+function volume_change() {
+    volume_show.innerHTML = recent_volume.value;
+    track.volume = recent_volume.value / 100;
+}
+
+//mudança posição slider
+function change_duration() {
+    slider_position = track.duration * (slider.value / 100);
+    track.currentTime = slider_position;
+}
+
+//funcao do autoplay
+function autoplay_switch() {
+    if (autoplay == 1) {
+        autoplay = 0;
+        auto_play.style.background = "rgba(255,255,255,0.2)";
+    } else {
+        autoplay = 1;
+        auto_play.style.background = "#FF8A65"
+    }
+}
+
+
+function range_slider() {
+    let position = 0;
+    if (!isNaN(track.duration)) {
+        position = track.currentTime * (100 / track.duration);
+        slider.value = position;
+    }
+
+    //funcao para rodar quando a musica acabar
+    if (track.ended) {
+        play.innerHTML = '<i class="fa-solid fa-play"></i>';
+        if (auto_play == 1) {
+            index_no += 1;
+            load_track(index_no);
+            playsong();
+        }
+    }
 }
